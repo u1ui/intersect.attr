cabe26719097f6e9cf2837da4d246e70f1af44bb
Tobias Buschor <tobias.buschor@shwups.ch>
2021-10-28 03:32:32 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# intersected.attr
+# intersect.attr
+declarative intersection observer
+
+# Ussage
+
+```html
+<div u1-intersect id=myEl></div>
+
+<style>
+#myEl.u1-intersected { /* triggers by default at threshold of 0 */
+    background:green;
+}
+#myEl {
+    --u1-intersect-threshold:0.2;  /* set a custom threshold */
+    --u1-intersect-margin:-20%;  /* and a margin */
+}
+#myEl[u1-intersected~="1"] { /* use a one of the predefined thresholds "0" "0.5" or "1" */
+    background:red;
+}
+</style>
+```
+
+
+## Demo
---
+++ intersect.js
@@ -0,0 +1,41 @@
+/* Copyright (c) 2016 Tobias Buschor https://goo.gl/gl0mbf | MIT License https://goo.gl/HgajeK */
+
+import 'https://cdn.jsdelivr.net/npm/wicked-elements@3.1.2/min.js';
+
+
+wickedElements.define(
+    '[u1-intersect]', {
+        init() {},
+        connected() {
+            const el = this.element;
+            const style = getComputedStyle(el);
+            const threshold = parseFloat(style.getPropertyValue('--u1-intersect-threshold').trim()) || 0;
+            const margin = style.getPropertyValue('--u1-intersect-margin') || '0px';
+
+            const thresholds = new Set([threshold, 0, 1, 0.5]);
+
+            this.u1IntersectionObserver = new IntersectionObserver(entries=>{
+                let attrValue = '';
+                let ratio = entries[0].intersectionRatio;
+                let onTarget = false;
+                if (entries[0].isIntersecting) {
+                    for (const val of thresholds) {
+                        if (ratio >= val) attrValue += ' '+val;
+                    }
+                    if (ratio >= threshold) onTarget = true;;
+                }
+                attrValue = attrValue.trim();
+				el.setAttribute('u1-intersect', attrValue);
+                el.classList[onTarget?'add':'remove']('u1-intersected');
+			}, {
+				//root: document.scrollingElement,
+				rootMargin: margin,
+				threshold: thresholds,
+			});
+            this.u1IntersectionObserver.observe(el);
+        },
+        disconnected() {
+            this.u1IntersectionObserver.unobserve(el);
+        },
+    }
+);
